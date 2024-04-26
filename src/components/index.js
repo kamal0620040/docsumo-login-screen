d2f4bd375471f293993dd822546418c5f5005bba
kamal0620040 <neupane0620040@outlook.com>
2024-04-26 15:48:20 +0545

    Fix: CSS issue in components

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Button from "./Button/Button";
+import Card from "./Card/Card";
+import Divider from "./Divider/Divider";
+import Input from "./Input/Input";
+import Link from "./Link/Link";
+
+export {Link, Input, Divider, Card, Button};
