ffeb01806fcaf0e271c9311cb04f2d3fe1536485
kamal0620040 <neupane0620040@outlook.com>
2024-04-29 23:07:03 +0545

    Set proxy in vite config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
     "react/prop-types": "off",
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
-      'warn',
+      'ignore',
       { allowConstantExport: true },
     ],
   },
